9e27f3f584792b2a8ede6f8e76be3a5ba8527647
lsandakit <lsandakit>
2008-06-25 08:36:26 +0000

    Apply the improvement of keeping the clean imple of WebServiceClientImpl of Axis2. For this move the utility metods away from the iimplementation class.
    Trial fix. I have tested this on my local machine with the current codebase.

--- bundles/org.eclipse.jst.ws.axis2.consumption.ui/src/org/eclipse/jst/ws/internal/axis2/consumption/ui/wsrt/Axis2WebServiceClient.java
+++ bundles/org.eclipse.jst.ws.axis2.consumption.ui/src/org/eclipse/jst/ws/internal/axis2/consumption/ui/wsrt/Axis2WebServiceClient.java
@@ -14,16 +14,11 @@
  * 20070518   187311 sandakith@wso2.com - Lahiru Sandakith, Fixing test resource addition
  * 20080620   192527 samindaw@wso2.com - Saminda Wijeratne, Update the model information with the axis2 preference settings
  * 20080621   210817 samindaw@wso2.com - Saminda Wijeratne, Setting the proxyBean and proxyEndPoint values
+ * 20080625   210817 samindaw@wso2.com - Saminda Wijeratne, Setting the proxyBean and proxyEndPoint values - Refactoring
  *******************************************************************************/
 package org.eclipse.jst.ws.internal.axis2.consumption.ui.wsrt;
 
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.Vector;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.jst.ws.axis2.consumption.core.command.Axis2ClientCodegenCommand;
@@ -33,6 +28,7 @@ import org.eclipse.jst.ws.axis2.consumption.core.command.Axis2ClientTestCaseInte
 import org.eclipse.jst.ws.axis2.consumption.core.command.Axis2WebservicesServerCommand;
 import org.eclipse.jst.ws.axis2.consumption.core.data.DataModel;
 import org.eclipse.jst.ws.axis2.core.context.PersistentAxis2EmitterContext;
+import org.eclipse.jst.ws.axis2.core.utils.Axis2CoreUtils;
 import org.eclipse.jst.ws.internal.axis2.consumption.ui.task.DefaultsForHTTPBasicAuthCommand;
 import org.eclipse.wst.command.internal.env.core.ICommandFactory;
 import org.eclipse.wst.command.internal.env.core.SimpleCommandFactory;
@@ -43,10 +39,6 @@ import org.eclipse.wst.ws.internal.wsrt.AbstractWebServiceClient;
 import org.eclipse.wst.ws.internal.wsrt.IContext;
 import org.eclipse.wst.ws.internal.wsrt.ISelection;
 import org.eclipse.wst.ws.internal.wsrt.WebServiceClientInfo;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.xml.sax.SAXException;
 
 public class Axis2WebServiceClient extends AbstractWebServiceClient {
 
@@ -103,89 +95,15 @@ public class Axis2WebServiceClient extends AbstractWebServiceClient {
 		String fileName;
 		//Get the valid filename
 		fileName=this.getWebServiceClientInfo().getWsdlURL();
-		fileName=fileName.substring(5,fileName.length());
 
-		try {
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			dbf.setIgnoringElementContentWhitespace(true);
-			dbf.setNamespaceAware(false);
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			Document doc;
-			doc = db.parse(fileName);
-			Element documentElement = doc.getDocumentElement();
-			String serviceName="";
-			HashMap<String, String> portElements=new HashMap<String, String>();
-			String proxyEndPoint=null;
-
-			//Iterate the root element children to find service nodes
-			for (int i = 0; i < documentElement.getChildNodes().getLength(); i++) {
-				Node serviceElement = documentElement.getChildNodes().item(i);
-				if (serviceElement.getNodeName().equals("wsdl:service")){
-					serviceName=serviceElement.getAttributes().getNamedItem("name").getNodeValue();
-
-					//iterate the service node children to find wsdl:port nodes
-					for (int j = 0; j < serviceElement.getChildNodes().getLength(); j++) {
-						Node portElement = serviceElement.getChildNodes().item(j);
-						if (portElement.getNodeName().equals("wsdl:port")){
-							String portBinding=portElement.getAttributes().getNamedItem("binding").getNodeValue().toUpperCase();
-
-							//iterate the port node children to find the soap address node
-							for (int k = 0; k < portElement.getChildNodes().getLength(); k++) {
-								Node soapElement = portElement.getChildNodes().item(k);
-								if (!soapElement.getNodeName().equals("#text")){
-									String soapLocation=soapElement.getAttributes().getNamedItem("location").getNodeValue();
-									while (portElements.containsKey(portBinding))
-										portBinding="K"+portBinding;
-									portElements.put(portBinding, soapLocation);
-								}
-							}
-						}
-					}
-				}
-			}
-			String portBindType="";
-			String soap11B="SOAP11Binding".toUpperCase();
-			String soap12B="SOAP12Binding".toUpperCase();
-			String httpB="HttpBinding".toUpperCase();
-			String https="https";
-			String Stub="Stub";
-
-			//iterating through all found end points to determine the required endpoint in the order soap11, soap12, http
-			for (String string : portElements.keySet()) {
-				if (proxyEndPoint==null){
-					proxyEndPoint=portElements.get(string);
-					portBindType=string;
-				}
-				if (string.endsWith(soap11B)){
-					if (proxyEndPoint.startsWith(https)||(!portBindType.endsWith(soap11B))){
-						proxyEndPoint=portElements.get(string);
-						portBindType=string;
-					}
-				}
-				if ((!portBindType.endsWith(soap11B))&&(string.endsWith(soap12B))){
-					if (proxyEndPoint.startsWith(https)||(!portBindType.endsWith(soap12B))){
-						proxyEndPoint=portElements.get(string);
-						portBindType=string;
-					}
-				}
-				if (!((portBindType.endsWith(soap11B))||(portBindType.endsWith(soap12B)))&&(string.endsWith(httpB))){
-					if (proxyEndPoint.startsWith(https)||(!portBindType.endsWith(httpB))){
-						proxyEndPoint=portElements.get(string);
-						portBindType=string;
-					}
-				}
-			}
-			if (proxyEndPoint!=null){
-				String proxyBean=serviceName+Stub;
-				axis2ClientOutputCommand.setProxyBean(proxyBean);
-				axis2ClientOutputCommand.setProxyEndpoint(proxyEndPoint);
-			}
-		} catch (SAXException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
+		String Stub="Stub";
+		String serviceName=Axis2CoreUtils.getServiceNameFromWSDL(fileName);
+		String proxyEndPoint=Axis2CoreUtils.getServiceEndPointFromWSDL(fileName, serviceName);
+		
+		if (proxyEndPoint!=null){
+			String proxyBean=serviceName+Stub;
+			axis2ClientOutputCommand.setProxyBean(proxyBean);
+			axis2ClientOutputCommand.setProxyEndpoint(proxyEndPoint);
 		}
 	}
 		
